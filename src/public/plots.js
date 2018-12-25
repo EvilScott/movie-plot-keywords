94140e4f36e59b3d6d80e0b4b533593af242a137
EvilScott <reis.robert.s@gmail.com>
2018-12-24 20:32:17 -0800

    hook up search box

--- src/public/plots.js
+++ src/public/plots.js
@@ -2,6 +2,14 @@
   const decoder = new TextDecoder('utf8');
   const decode = (value) => decoder.decode(value, { stream: true });
 
+  const searchForm = document.getElementById('search');
+  const termInput = document.getElementById('term');
+
+  searchForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    await searchForTerm(termInput.value)
+  });
+
   const createMovieBox = (movie) => {
     let movieBox = document.createElement('div');
     movieBox.setAttribute('class', 'movie box');
@@ -14,7 +22,7 @@
     h4.setAttribute('class', 'subtitle');
     h4.append(document.createTextNode(movie.year));
 
-    let p = document.createElement('p');
+    let p = document.createElement('p'); //TODO nl2br
     p.setAttribute('class', 'content');
     p.append(document.createTextNode(movie.plot));
 
@@ -27,23 +35,29 @@
     return movieBox;
   };
 
-  let movieTarget = document.getElementById('results');
-  movieTarget.innerHTML = '';
+  const searchForTerm = async (term) => {
+    const movieTarget = document.getElementById('results');
+    movieTarget.innerHTML = '';
+    // TODO set loading
 
-  const res = await fetch('/api/movies');
-  const reader = res.body.getReader();
+    const res = await fetch(`/api/movies/search/${term}`);
+    const reader = res.body.getReader();
 
-  hl(async (push, next) => {
-    const { done, value } = await reader.read();
-    if (!done) {
-      push(null, value);
-      return next();
-    }
-    push(null, hl.nil);
-  }).map(decode)
-    .split('\n')
-    .map(JSON.parse)
-    .map(createMovieBox)
-    .each(el => movieTarget.append(el));
+    hl(async (push, next) => {
+      const { done, value } = await reader.read();
+      if (!done) {
+        push(null, value);
+        return next();
+      }
+      push(null, hl.nil);
+    }).map(decode)
+      .split('\n')
+      .map(JSON.parse)
+      .map(createMovieBox)
+      .each(el => {
+        // TODO turn off loading
+        movieTarget.append(el);
+      });
+  };
 
 })(highland);
